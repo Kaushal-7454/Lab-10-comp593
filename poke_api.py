6b322943e745238b34114e95e5a6a767dfae5b9a
Kaushal-7454 <133620623+Kaushal-7454@users.noreply.github.com>
2023-08-11 18:44:56 -0800

    final script
    whole script has completed there are some steps are nit completed and few errors in this code.

--- poke_api.py
+++ poke_api.py
@@ -47,8 +47,28 @@ def get_pokemon_info(pokemon):
         print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
 
     # TODO: Define function that gets a list of all Pokemon names from the PokeAPI
+def fetch_all_pokemon_name():
+    """
+    This function qill help to fetch all the names of the Pokemon names.
 
+    Returns:
+        Whole List: it will return list of pokemon names otherwise it will return 
+        error message or enpty list.
+    """
+    whl_url = POKE_API_URL + "?limit=1000"
+    response_msg = requests.get(whl_url)
+
+    if response_msg.status_code = requests.code.ok:
+        pokemon_data = response_msg.json()
+        st_pokemon_name = [entry['NAME'] for entry in pokemon_data['results']]
     # TODO: Define function that downloads and saves Pokemon artwork
+def save_pokemon_artwork(poke_name, store_path):
+    poke_name = poke_name.strip().lower()
 
+    if poke_name == '':
+        print("Failed: pokemon name has not provided.")
+        return False 
+    poke_url = POKE_API_URL + f"{poke_name}/"
+    response_msg = requests.get(poke_url)
 if __name__ == '__main__':
     main()
